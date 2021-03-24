133e219062d1c14a72b67ec823e58ef1a4f7b325
Allewe <badraliyoumaiga199@gmail.com>
2021-03-24 19:49:45 +0100

    navbar et boite modal avec les elements

--- components/modal.js
+++ components/modal.js
@@ -1,20 +1,32 @@
 /**
  * @class Modal
+ *
  */
 
 export default class Modal extends HTMLElement{
+    
+        
 
     constructor(){
         super()
 
         //Shadow DOM
         this.modal = this.attachShadow({ mode : 'open'})
+        
         //contenu de la boite modal
+        //recuperation des items 
+        this.jsModal = document.querySelector(".header-left")
+        console.log(this.jsModal.innerHTML)
+        
+        
+        
+        
+
         this.modal.innerHTML = `
             <style>
                 div{
                     display : flex;
-                    background : rgba(157, 170, 42, 0.8);
+                    background : rgba(0, 0, 0, 0.5);
                     align-items : center;
                     justify-content : center;
                     left : 0;
@@ -23,9 +35,16 @@ export default class Modal extends HTMLElement{
                     z-index: 10000;
                     width : 100vw;
                     height : 100vh;
+                    flex-direction : column;
+                    }
+                    li{
+                        list-style : none;
                     }
             </style>
-            <div>Hello World</div>
+            <div>
+                ${this.jsModal.innerHTML}
+            </div>
         `
     }
+   
 }
